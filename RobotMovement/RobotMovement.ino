b1675a4cecb98c5b76e39759ffc92a748f635e7e
Sharwin Patil <sharwin24@gmail.com>
2022-02-27 16:29:47 -0500

    Added truth tables

--- RobotMovement/RobotMovement.ino
+++ RobotMovement/RobotMovement.ino
@@ -7,6 +7,15 @@
 
 #include <Servo.h>
 
+//     Left motor truth table
+// Here are some handy tables to show the various modes of operation.
+//  ENA         IN3             IN4         Description
+//  LOW   Not Applicable   Not Applicable   Motor is off
+//  HIGH        LOW             LOW         Motor is stopped (brakes)
+//  HIGH        LOW             HIGH        Motor is on and turning forwards
+//  HIGH        HIGH            LOW         Motor is on and turning backwards
+//  HIGH        HIGH            HIGH        Motor is stopped (brakes)
+
 //     Right motor truth table
 // Here are some handy tables to show the various modes of operation.
 //  ENB         IN3             IN4         Description
