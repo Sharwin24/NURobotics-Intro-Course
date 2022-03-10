8f4961fd4843ef9a075cf92d1f9d1db182257076
Sharwin Patil <sharwin24@gmail.com>
2022-03-10 17:58:25 -0500

    Added movement functions for line follower demo

--- LineFollowerDemo/LineFollowerDemo.ino
+++ LineFollowerDemo/LineFollowerDemo.ino
@@ -19,9 +19,85 @@
 // Variables for tracking the state of the robot, timers, etc.
 unsigned long startTime;
 
-/*
-    Copy over the movement functions from RobotMovement.ino
-*/
+// Movement functions from RobotMovement
+
+/**
+ * @brief Moves the robot forward with the given speed
+ *
+ * @param carSpeed an integer representing the car's speed.
+ * This value is capped at 255 and passing in a higher value will
+ * cap at 255
+ */
+void forward(int carSpeed)  // Forward
+{
+    // Setting Direction and Power Pins
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    // Write speed to 'A' and 'B' MotorGroups
+    analogWrite(ENA, carSpeed);
+    analogWrite(ENB, carSpeed);
+}
+
+/**
+ * @brief Moves the robot backwards with the given speed
+ *
+ * @param carSpeed an integer representing the car's speed.
+ * This value is capped at 255 and passing in a higher value will
+ * cap at 255
+ */
+void back(int carSpeed) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, carSpeed);
+    analogWrite(ENB, carSpeed);
+}
+
+/**
+ * @brief Turns the robot left with the given speed
+ *
+ * @param carSpeed an integer representing the car's speed.
+ * This value is capped at 255 and passing in a higher value will
+ * cap at 255
+ */
+void turnLeft(int carSpeed) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, carSpeed);
+    analogWrite(ENB, carSpeed);
+}
+
+/**
+ * @brief Turns the robot right with the given speed
+ *
+ * @param carSpeed an integer representing the car's speed.
+ * This value is capped at 255 and passing in a higher value will
+ * cap at 255
+ */
+void turnRight(int carSpeed) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, carSpeed);
+    analogWrite(ENB, carSpeed);
+}
+
+/**
+ * @brief Stops the robot's drive motors.
+ *
+ */
+void stop() {
+    digitalWrite(ENA, 0);
+    digitalWrite(ENB, 0);
+}
+
+// End movement functions from RobotMovement
 
 // Setup Pinmodes for sensor
 void setup() {
